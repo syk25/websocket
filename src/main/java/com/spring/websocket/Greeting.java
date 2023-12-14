713fe46ec0af3ddfc7950f7b03e02cb4eb565435
syk25 <syk2245@gmail.com>
2023-12-14 17:26:47 +0900

    websocket tutorial

---
+++ src/main/java/com/spring/websocket/Greeting.java
@@ -0,0 +1,12 @@
+package com.spring.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/com/spring/websocket/HelloMessage.java
@@ -0,0 +1,15 @@
+package com.spring.websocket;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+    private String name;
+}
